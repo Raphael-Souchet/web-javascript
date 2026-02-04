d85cd29fda58956dcf7cafeefeb84dd2b0257608
Raphael <raphael.souchet.85@gmail.com>
2026-02-04 11:38:21 +0100

    TP5

--- Hello_World/2-Les_variables/monscript.js
+++ Hello_World/2-Les_variables/monscript.js
@@ -1,12 +1,26 @@
-let nomJour = "mercredi";
 let numJour = "04";
-let nomMois = "janvier";
 let isHiver = true;
+
+let tabJours = ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"]; 
+let tabMois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"]; 
+
+let maDateV1 = {
+    "jour" : tabJours[2],
+    "mois" : tabMois[0]
+}
+
+let maDateV2 = {};
+maDateV2.jour = tabJours[2];
+maDateV2.mois = tabMois[0];
+
 
 console.log();
 if (isHiver) {
-    console.log(`Nous sommes le ${nomJour} ${numJour} ${nomMois} et nous sommes en hiver`);
+    console.log(`Nous sommes le ${tabJours[2]} ${numJour} ${tabMois[0]} et nous sommes en hiver`);
 }
 else {
-    console.log(`Nous sommes le ${nomJour} ${numJour} ${nomMois} et nous sommes en pas en hiver`);
+    console.log(`Nous sommes le ${tabJours[2]} ${numJour} ${tabMois[0]} et nous sommes en pas en hiver`);
 }
+
+console.log(maDateV1);
+console.log(maDateV2);
