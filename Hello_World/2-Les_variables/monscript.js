0dec138febc230fcf2415c06217cc57fa171f80f
Raphael <raphael.souchet.85@gmail.com>
2026-02-04 11:28:27 +0100

    TP4

---
+++ Hello_World/2-Les_variables/monscript.js
@@ -0,0 +1,12 @@
+let nomJour = "mercredi";
+let numJour = "04";
+let nomMois = "janvier";
+let isHiver = true;
+
+console.log();
+if (isHiver) {
+    console.log(`Nous sommes le ${nomJour} ${numJour} ${nomMois} et nous sommes en hiver`);
+}
+else {
+    console.log(`Nous sommes le ${nomJour} ${numJour} ${nomMois} et nous sommes en pas en hiver`);
+}
