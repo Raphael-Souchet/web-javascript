7174bec44b0d7ae07511b8190295d671f839da37
Raphael <raphael.souchet.85@gmail.com>
2026-02-25 11:28:45 +0100

    TP15

--- Hello_World/5-Les_manipulations/TP13/monscript.js
+++ Hello_World/5-Les_manipulations/TP13/monscript.js
@@ -1,6 +1,14 @@
 /* Toutes les variables */
 let li = document.querySelector("#listeCourses li");
 
+let dateJour = new Date(Date.now());
+let dateJourFr = dateJour.toLocaleDateString('fr-FR');
+
+let titreH2 = document.querySelector("h2");
+
+titreH2.textContent = titreH2.textContent + " : " + dateJourFr;
+
+
 /* Tous les évènements */
 li.addEventListener('click', ()=>{
   li.classList.toggle("itemCheck");
